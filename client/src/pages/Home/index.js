00ec36b28f4355f72254f2d1aacea7fdfec7fa97
Marcia Moss <marcia.moss@ibm.com>
2021-04-17 21:56:16 -0400

    remove gitignore files

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
 
-function Home() {
-
-  return (
-      <div className="container">
-          <h1>Home Page</h1>
-      </div>
-  );
+class Home extends Component {
+  render() {
+    return (
+        <div className="container">
+            <form className="mt-5">
+              <div className="row">
+                <div className="col-7">
+                  <label>Type in a song to search</label>
+                  <input type="text" className="form-control textBoxStyle" placeholder="Song"/>
+                </div>
+              </div>
+            </form>
+        </div>
+    );
+  }
 }
 
 export default Home;
